18f0e805ffbb5ad01c747d41a8aea754f52efd40
diuguide <everett.diuguid@gmail.com>
2022-08-20 01:40:45 -0400

    tic tac toe board set

---
+++ bash/tictactoe/main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "    1  2  3 "
+echo "   -- -- -- "
+echo "A |  |  |  |"
+echo "   -- -- -- "
+echo "B |  |  |  |"
+echo "   -- -- -- "
+echo "C |  |  |  |"
+echo "   -- -- -- "
+
+exit
+
+
