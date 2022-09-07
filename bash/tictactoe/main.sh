e9bf435ce991b1817c8d9f3376087800476dbd41
diuguide <everett.diuguid@gmail.com>
2022-09-06 23:27:59 -0400

    clean up

--- bash/tictactoe/main.sh
+++ bash/tictactoe/main.sh
@@ -8,15 +8,15 @@ d=`date '+H%m%s'`
 
 mkdir temp
 
-echo -e $playervar >> "./temp/moves$d.txt"
+echo -e $playervar > "./temp/moves.txt"
 
 ./render.sh 
 
 read -p "Player move..." playervar
 
-echo -e $playervar >> "./temp/moves$d.txt"
+echo -e $playervar >> "./temp/moves.txt"
 
-mv  "./temp/moves$d.txt"  "./hist/moves$d.txt"
+mv  "./temp/moves.txt"  "./hist/moves$d.txt"
 
 rm -r ./temp
 
