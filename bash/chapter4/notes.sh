1baa2ceff9cf0d245e048e2cd37e409b1bfc58c2
diuguide <everett.diuguid@gmail.com>
2022-09-30 23:45:44 -0400

    end of ch4

--- bash/chapter4/notes.sh
+++ bash/chapter4/notes.sh
@@ -56,5 +56,32 @@ echo $a # Unquoted, however, removes tabs and newlines.
 echo
 echo "$a" # The quoted variable preserves whitespace.
  # (See the chapter on "Quoting.")
+
+# eval arguments are concatonated as a string and read as a command
+# sed is a stream editor for bash, refer to man pages for details
+
+#positional parameters
+# arguments passed to the script from the command line: $0, $1, $2, $3 . . .
+# $0 is the name of the script itself, $1 is the first argument, $2 the second, $3 the third, and so forth.
+# [13] After $9, the arguments must be enclosed in brackets, for example, ${10}, ${11}, ${12}.
+
+# The special variables $* and $@ denote all the positional parameters.
+
+# if [ −n "${10}" ] # Parameters > $9 must be enclosed in {brackets}.
+# then
+#  echo "Parameter #10 is ${10}"
+# fi 
+
+# symbolic links
+
+# The shift command reassigns the positional parameters, in effect shifting them to the left one notch.
+# $1 <−−− $2, $2 <−−− $3, $3 <−−− $4, etc.
+# The old $1 disappears, but $0 (the script name) does not change. If you use a large number of
+# positional parameters to a script, shift lets you access those past 10, although {bracket} notation also
+# permits this
+
+
+
+
 exit 0
 
